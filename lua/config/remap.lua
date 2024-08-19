b918fd216c780e9527602d45db1c7c4404e5c936
Marcus Hult <marcushult@hotmail.se>
2024-08-20 00:31:30 +0200

    fix keymaps for lsp so it is like kickstart

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -65,16 +65,35 @@ vim.keymap.set('n', '<leader>xQ', '<cmd>Trouble qflist toggle<cr>')
 -- if there is a language server active in the file
 local function on_lsp_attach(_, bufnr)
     local opts = { buffer = bufnr }
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = '[R]e[n]ame' })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = '[C]ode [A]ction' })
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = '[G]oto [D]efinition' })
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references,
+        { buffer = bufnr, desc = '[G]oto [R]eferences' })
+    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = '[G]oto [I]mplementation' })
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'Type [D]efinition' })
+    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols,
+        { buffer = bufnr, desc = '[D]ocument [S]ymbols' })
+    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+        { buffer = bufnr, desc = '[W]orkspace [S]ymbols' })
+
+    -- See `:help K` for why this keymap
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover Documentation' })
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'Signature Documentation' })
+
+    -- Lesser used LSP functionality
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = '[G]oto [D]eclaration' })
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {
+        buffer = bufnr,
+        desc =
+        '[W]orkspace [A]dd Folder'
+    })
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
+        { buffer = bufnr, desc = '[W]orkspace [R]emove Folder' })
+    vim.keymap.set('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, { desc = '[W]orkspace [L]ist Folders' })
 end
 
 return { on_lsp_attach = on_lsp_attach }
